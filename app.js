a2aed602c70cf403d40b2ce6c4af217100dacfd8
saloni2811 <saloni28singh2001@gmail.com>
2023-03-14 21:59:25 +0530

    Some Changes

--- app.js
+++ app.js
@@ -3,33 +3,46 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Food" , "Cook Food" , "Eat food"];
-app.use(bodyParser.urlencoded({extended: true}));
+let items = ["Buy Food" , "Cook Food" , "Eat food"];
+let workItems = [];
+
 app.set("view engine" , "ejs");
 
-app.get("/", function(req,res){
-    var today = new Date();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-    var options = {
+app.get("/", function(req,res){
+    let today = new Date();
+
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US" , options);
+    let day = today.toLocaleDateString("en-US" , options);
     
    
-    res.render("list" , {kindOfDay : day , newListItems : items});
+    res.render("list" , {listTitle : day , newListItems : items});
 });
 app.post("/" , function(req,res){
-    var item = req.body.newItem;
+    let item = req.body.newItem;
 
     items.push(item);
     
     res.redirect("/");
 
 });
+app.get("/work" , function(req , res){
+    res.render("list" , {listTitle : "Work List" , newListItems: workItems});
+});
 
+app.post("/work" , function(req,res){
+    let item = req.body,newItem;
+    workItems.push(item);
+    res.redirect("/work");
+
+})
 
 
 
